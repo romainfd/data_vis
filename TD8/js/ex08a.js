40c4fc186ccbbc1b5d51281c26a474aaef65d86a
Romain Fouilland <romain.fd@gmail.com>
2018-11-19 11:24:23 +0100

    Finished part a

--- TD8/js/ex08a.js
+++ TD8/js/ex08a.js
@@ -5,6 +5,61 @@ var createPlot = function(){
         "config": {
             "view": {"stroke": "transparent"}
         },
+        "layer": [
+            // THE MAP
+            {
+                "data": {
+                    "url": "us-10m.json",
+                    "format": {
+                        "type": "topojson",
+                        "feature": "states"
+                    }
+                },
+                "projection": {
+                    "type": "albersUsa"
+                },
+                "mark": {
+                    "type": "geoshape",
+                    "stroke": "grey"
+                }
+            },
+            {
+                "data": {
+                    "url": "airports.json"
+                },
+                "transform": [{
+                    "lookup": "state",  // key in our data
+                    "from": {
+                        "data": {"url": "states_tz.csv"},  // JOIN WITH
+                        "key": "State",  // ON the foreign key
+                        "fields": ["TimeZone"]  // SELECT what
+                    }
+                }, {
+                    "filter": "!test(regexp(/[0-9]/), datum.iata)"  // we remove the airport with with a number
+                    // based on {"filter": {"field": "height", "lt": 180}}
+                }],
+                "projection": {
+                    "type": "albersUsa"
+                },
+                "mark": "point",
+                "encoding": {
+                    "longitude": {
+                        "field": "longitude",
+                        "type": "quantitative"
+                    },
+                    "latitude": {
+                        "field": "latitude",
+                        "type": "quantitative"
+                    },
+                    "size": {"value": 10},
+                    "color": {
+                        "field": "TimeZone",
+                        "type": "nominal",
+                        "legend": false
+                    }
+                }
+            }
+        ]
     };
     vlOpts = {width:1000, height:600, actions:false};
     vegaEmbed("#map", vlSpec, vlOpts);
